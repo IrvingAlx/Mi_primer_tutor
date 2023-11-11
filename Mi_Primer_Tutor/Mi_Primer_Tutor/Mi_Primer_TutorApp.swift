ea745eb01f4c1eed436331fce5fed7b5dac4c252
IrvingAlx <irvingalejandrovl@gmail.com>
2023-11-11 15:27:08 -0600

    Project Kickoff: "My First Tutor"

---
+++ Mi_Primer_Tutor/Mi_Primer_Tutor/Mi_Primer_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mi_Primer_TutorApp.swift
+//  Mi_Primer_Tutor
+//
+//  Created by Irving Alejandro Vega Lagunas on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Mi_Primer_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
